244bb92db430b9399ccc77c00b4010d7bb77520f
andres <fcortesbio@gmail.com>
2025-08-03 00:29:56 -0500

    This sets up:
    traefik: reverse proxy
    
    whoami: a tiny test container that echoes client info (intended for testing routing)

---
+++ test-node-api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello from your Node.js API!" });
+});
+
+app.get("/status", (req, res) => {
+  res.send({ status: "ok", time: new Date() });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
